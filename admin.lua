5e267723de9653b529378413a8677f72a39dec67
santos71heitor-hub <santos71heitor@gmail.com>
2025-09-19 22:40:47 -0300

    Update admin.lua

--- admin.lua
+++ admin.lua
@@ -171,7 +171,7 @@ local function createPlayerESP(player)
         local label = Instance.new("TextLabel")
         label.Size = UDim2.new(1,0,1,0)
         label.BackgroundTransparency = 1
-        label.TextColor3 = Color3.fromRGB(255,255,255)
+        label.TextColor3 = Color3.fromRGB(0,0,0)
         label.TextStrokeTransparency = 0
         label.TextStrokeColor3 = Color3.new(255,255,255)
         label.Font = Enum.Font.GothamBold
